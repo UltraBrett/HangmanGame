fa04ec7b07094f7493ca7694136fa0eb131c2752
UltraBrett <brett.stanaland@geofeedia.com>
2015-03-12 13:39:49 -0400

    Shell classes for all automated requirements

---
+++ HangmanGame/HangmanGame/GameTestClass.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace HangmanGame
+{
+	[TestFixture]
+	public class GameTestClass
+	{
+		//Requirement #1
+		[Test]
+		public void PassedDataEqualsIntentData ()
+		{
+			Assert.False (true);
+		}
+
+		//Requirement #2
+		[Test]
+		public void CloseOnNullData ()
+		{
+			Assert.False (true);
+		}
+
+		//Requirement #3
+		[Test]
+		public void CorrectCharacters ()
+		{
+			Assert.False (true);
+		}
+
+		//Requirement #5
+		[Test]
+		public void CorrectStartState ()
+		{
+			Assert.False (true);
+		}
+
+		//Requirement #7
+		[Test]
+		public void CharacterBelongsToTerm ()
+		{
+			Assert.False (true);
+		}
+
+		//Requirement #8
+		[Test]
+		public void PenaltyForWrongCharacter ()
+		{
+			Assert.False (true);
+		}
+
+		//Requirement #10
+		[Test]
+		public void VerifyWinningState ()
+		{
+			Assert.False (true);
+		}
+
+		//Requirement #11
+		[Test]
+		public void VerifyLosingState ()
+		{
+			Assert.False (true);
+		}
+
+		//Requirement #13
+		[Test]
+		public void UseSameCharacterAndNoPenalty ()
+		{
+			Assert.False (true);
+		}
+	}
+}
+
